72d696380684db881db0ea47356283271c5f5316
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-11-29 15:19:47 +0500

    updated

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p>Wow</p>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/main.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client'
+//styles
+import './index.css'
+//App Entry
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+
+    <App />
+  
+)
