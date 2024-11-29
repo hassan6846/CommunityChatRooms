67d4d3bf75721eb5fd5bc2649c3d88f1ebd15f6a
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-11-29 16:53:29 +0500

    main

--- client/src/routes/Routing.tsx
+++ client/src/routes/Routing.tsx
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Route, Routes, } from "react-router-dom";
 
 import Login from "../pages/Login/Login";
 import Signup from "../pages/Signup/Signup";
-
 import Error from "../pages/Error/Error";
 import Home from "../pages/Sidebar/AdminOutlet";
 
@@ -14,11 +13,10 @@ const Routing = () => {
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} >
-                
                 </Route>
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
-                <Route path="*" element={<Error/>}/>
+                <Route path="*" element={<Error />} />
             </Routes>
         </Router>
     )
