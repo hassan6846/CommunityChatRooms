3a10123b1d37073e3fe566b4586b59070fc2f35b
Hassan <96440779+hassan6846@users.noreply.github.com>
2025-01-04 15:41:13 +0500

    Initialize backend and client structure with basic configurations and files

---
+++ backend/app.js
@@ -0,0 +1,42 @@
+const app = require('express')();
+const http=requrie('http')
+const server=http.createServer(app)
+
+
+
+//packages
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+
+//middlewares
+app.set('trust proxy', 1) // trust first proxy
+app.disable('x-powered-by')
+app.use(cors())
+app.use(helmet())
+app.use(morgan('dev'))
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!")
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+})
+
+
+
+//routes
+
+
+//server
+
+
+//export
+module.exports=app
+
+
