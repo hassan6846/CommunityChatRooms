673681a332e79d1dc65e0e93fcf82a906218cf4f
Hassan <96440779+hassan6846@users.noreply.github.com>
2025-01-04 18:27:30 +0500

    Add @mui/icons-material and formik dependencies for enhanced UI and form management

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,6 @@ app.use(express.json())
 app.use((req, res, next) => {
     res.status(404).send("Sorry can't find that!")
 })
-
 app.use((err, req, res, next) => {
     console.error(err.stack)
     res.status(500).send('Something broke!')
