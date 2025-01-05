2f9e0254ed641a83ee360225281efd6cb6b29a0e
Hassan <96440779+hassan6846@users.noreply.github.com>
2025-01-05 19:00:20 +0500

    feat:Added Navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 // import ForgotPassword from "./pages/ForgotPassword/ForgotPassword"
 // import Login from "./pages/Login/Login"
 import Home from "./pages/Home/Home"
-import ResetPassword from "./pages/ResetPassword/ResetPassword"
+// import ResetPassword from "./pages/ResetPassword/ResetPassword"
 // import Signup from "./pages/Signup/Signup"
 
 
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,8 +1,84 @@
 import React from 'react'
+//css
+import styles from "./Home.module.css"
+import { Avatar, IconButton, TextField, InputAdornment,  } from '@mui/material'
+import ViewSidebarIcon from '@mui/icons-material/ViewSidebar';
+import { Search} from '@mui/icons-material';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className={styles.HomeContainer}>
+      <div className={styles.Sidebar}>
+
+
+
+        <div className={styles.iconContainer}>
+          <Avatar sx={{
+            objectFit: "contain"
+          }} variant="rounded" src='https://images.seeklogo.com/logo-png/44/1/stackblitz-icon-logo-png_seeklogo-444239.png?v=1957148091787478992' />
+
+
+          <IconButton sx={{ color: '#CFD4D8' }}>
+            <ViewSidebarIcon />
+          </IconButton>
+
+
+        </div>
+        <TextField
+          fullWidth
+          margin="normal"
+          name="Search Rooms"
+          placeholder='search'
+          InputProps={{
+            style: {
+              fontFamily: "Outfit",
+              height: "50px",
+              borderColor: "#CFD4D8",
+              borderRadius: "10px"
+            },
+            startAdornment: (
+              <InputAdornment position="start">
+                <Search sx={{ color: '#CFD4D8' }} />
+              </InputAdornment>
+            ),
+
+          }}
+        />
+        {/* Only Contain Active Links */}
+        <div>
+        <p style={{fontSize:"14px",color:"#acb1b5"}}>#Rooms</p>
+
+
+        </div>
+        {/* Only Contain Active Links */}
+    <div>
+
+  
+        <div className={styles.LogoutWrapper}>
+        <p style={{fontSize:"14px",color:"#acb1b5"}}>More</p>
+
+     
+        </div>
+        <div className={styles.profileBtn}>
+          
+          <Avatar src='https://avatar.iran.liara.run/public' />
+        
+          <div >
+            
+            <p>UserName</p>
+            <p>User Type</p>
+          </div>
+
+        </div>
+        </div>
+      </div>
+
+      <nav className={styles.Navbar}>
+        <p>Wow</p>
+      </nav>
+
+  
+    </div>
   )
 }
 
